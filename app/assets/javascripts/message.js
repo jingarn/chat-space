c39b2577c8cb239bd1f350365bfac1ff5d688730
jingarn <jingarn+github@gmail.com>
2020-07-19 21:01:31 +0900

    devel message ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,20 @@
 $(function(){
+  function buildMessage(message){
+    let html = `<div class="Chat-main__message-list__info">
+                  <div class="Chat-main__message-list__info__name">
+                    ${message.user_name}
+                  </div>
+                  <div class="Chat-main__message-list__info__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="Chat-main__message-list__text">
+                  ${message.content}`;
+    if(message.image)
+      html += `<img class="Message__image" src="${message.image}">`;
+    html += `</div>`;
+    return html
+  }
   $('.Chat-main__message-form__form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -11,5 +27,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      let html = buildMessage(message)
+      $('.Chat-main__message-list').append(html)
+      $('.Chat-main__message-list').animate({scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.Chat-main__message-form__form__send-btn').prop("disabled", false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
 });
