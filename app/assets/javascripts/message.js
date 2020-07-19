52bf1035d1f225093682dd1b90007ca297bbb10c
jingarn <jingarn+github@gmail.com>
2020-07-19 18:38:00 +0900

    commit #6

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.Chat-main__message-form__form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+});
